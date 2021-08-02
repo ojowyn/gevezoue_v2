31f4d84c5cd3b2144faeed70b162ca2fe1d174a9
ojowyn <ddelepinegmail.com>
2021-08-02 19:36:41 +0200

    init project

---
+++ project/web/modules/gvz_config_override/src/Config/GvzConfigOverrideParagraph.php
@@ -0,0 +1,56 @@
+<?php
+namespace Drupal\gvz_config_override\Config;
+
+use Drupal\Core\Cache\CacheableMetadata;
+
+class GvzConfigOverrideParagraph implements \Drupal\Core\Config\ConfigFactoryOverrideInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function loadOverrides($names)
+    {
+        $override = [];
+        if (in_array('field.storage.paragraph.field_position_image', $names)) {
+          $override['field.storage.paragraph.field_position_image'] = [
+            'settings' => [
+              'allowed_values' => [],
+              'allowed_values_function' => [$this, 'fieldPositionImageList'],
+            ],
+          ];
+        }
+        return $override;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCacheSuffix()
+    {
+        return 'GvzConfigOverrideParagraph';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function createConfigObject($name, $collection = \Drupal\Core\Config\StorageInterface::DEFAULT_COLLECTION)
+    {
+        return NULL;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCacheableMetadata($name)
+    {
+      return new CacheableMetadata();
+    }
+
+    public function fieldPositionImageList() {
+      return [
+        'Center' => 'center',
+        'Gauche' => 'left'
+      ];
+    }
+}
